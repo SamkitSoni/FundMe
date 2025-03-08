9c77c64308c46d9fd4e07e706819c5b9c57f953b
SamkitSoni <samkitsoni09@gmail.com>
2025-03-08 23:33:47 +0530

    Added the utility that the withdraw function can only be used by the owner of the contact through Modifier

--- FundMe.sol
+++ FundMe.sol
@@ -13,13 +13,46 @@ contract FundME {
     uint256 public minUsd = 5e18;
 
     address[] public funders;
-    mapping(address funders => uint256 amountFunded) public addressToAmountFunded;
+    mapping(address => uint256 ) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+     }
 
     function fund() public payable {
         //Allow users to send $
         //Have a minimum $ sent
         require(msg.value.getConversionRate() >= minUsd, "didn't send enough ETH"); //1e18 = 1 * 10 ** 18 
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] = addressToAmountFunded[msg.sender];
+        addressToAmountFunded[msg.sender] += msg.value;
     }
- }
+
+    function withdraw() public onlyOwner {
+        for(uint256 funderIndex = 0 ; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        //reset the array
+        funders = new address[](0);
+        //actually withdraw the funds
+
+        // //msg.sender = address;
+        // //payable(msg.sender) = payable address
+        // //transfer
+        // payable(msg.sender).transfer(address(this).balance);//this refers to the whole contract
+        // //send(difference between send and transfer is that; transfer returns the error whereas the send returns the true or false value.
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send Fail"); 
+        // //call
+        (bool callSuccess, ) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call Failed");
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Must be Owner !");
+        _; //whatever you wanna do in the function
+        //order of this undersocre matters
+    }
+}
